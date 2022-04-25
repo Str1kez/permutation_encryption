8a932d989fadf374c306d892b51c3b43faeebc18
Str1kez <vazinov@yandex.ru>
2022-04-25 18:58:12 +0300

    check with nulls

--- base.py
+++ base.py
@@ -2,10 +2,22 @@ from itertools import zip_longest
 
 
 class Base:
-    def __init__(self, data, key, with_null) -> None:
+    def __init__(self, data, key, with_null, dtype=None) -> None:
+        self._dtype = dtype
         self._data = data
         self._key = key
         self._with_null = with_null
+        self._check_dtype()
+        
+    def _check_dtype(self) -> None:
+        if self._dtype:
+            if isinstance(self._dtype, tuple):
+                if self._dtype[0] == 'group':
+                    self._data = list(''.join(x) for x in grouper(self._data, self._dtype[1], incomplete='strict'))
+                else:
+                    raise ValueError('Your data type is not group')
+            elif (self._dtype == 'byte' or self._dtype == 'bit') and not isinstance(self._dtype, bytearray):
+                raise TypeError(f'It is not {self._dtype}')
 
 
 def grouper(iterable, n, *, incomplete='fill', fillvalue=None):
--- simple.py
+++ simple.py
@@ -11,7 +11,7 @@ class Simple(Base):
         return "_".join(map(str, elements))
     
     def __init__(self, data: str, key: str, **kwargs) -> None:
-        super().__init__(data, key, kwargs.get('with_null'))
+        super().__init__(data, key, kwargs.get('with_null'), kwargs.get('dtype'))
         self._key = self._key.split('_')
     
     def encryption(self) -> str:
@@ -19,24 +19,52 @@ class Simple(Base):
         grouped_data = grouper(self._data, len(self._key))
         result = ''
         for s in grouped_data:
+            # print(repr(s))
             result += ''.join(y for _, y in sorted((x, y) for x, y in zip(self._key, s)))
+        # print(repr(result))
         if not self._with_null:
             return result.replace('\0', '')
         return result
     
-    def decryption():
-        # ! Написать
-        pass
+    def decryption(self) -> str:
+        self.check_last_block()
+        grouped_data = list(grouper(self._data, len(self._key)))
+        result = ''
+        for s in grouped_data:
+            print(repr(s))
+            if s == grouped_data[-1] and self._with_null is False and any('\0' in x for x in s):
+                helper = list('\0' * len(s))
+                for i, key in enumerate(sorted(self._key[:-self._null_amount])):
+                    helper[int(key)] = s[i]
+                s = helper
+                # helper = list(s[:-self._null_amount])
+                # # print(repr(helper))
+                # for i in sorted(self._key[-self._null_amount:]):
+                #     helper.insert(int(i), s[-1])
+                # s = helper
+                print(repr(s))
+            result += ''.join([s[int(x)] for x in self._key])
+        if not self._with_null:
+            return result.replace('\0', '')
+        return result
         
     def check_last_block(self) -> None:
         if len(self._data) % len(self._key):
             if self._with_null is None:
                 exit('wrong settings for last block')
-            self._data += '\0' * (len(self._key) - len(self._data) % len(self._key))
+            self._null_amount = len(self._key) - len(self._data) % len(self._key)
+            if self._dtype and 'group' in self._dtype:
+                self._data += ['\0' * self._dtype[1] for _ in range(self._null_amount)]
+            else:
+                self._data += '\0' * (self._null_amount)
 
 
-key = Simple.generate_key(4)
-s = Simple('IlyaVazin', key, with_null=False)
-print(s._data)
-print(s._key)
-print(repr(s.encryption()))
+key = Simple.generate_key(20)
+encryption = Simple('IlyaVazinovV', key, with_null=True)
+# print(encryption._data)
+print(encryption._key)
+encrypted_data = encryption.encryption()
+print(repr(encrypted_data))
+decryption = Simple(encrypted_data, key, with_null=True)
+# print(repr(decryption._data))
+print(repr(decryption.decryption()))
